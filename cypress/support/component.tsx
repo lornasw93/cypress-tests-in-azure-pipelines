3462dc02917dca43f0b91c8e8478e06e6404baff
Lorna Watson <lorna@avenue3.com>
2023-07-06 23:07:38 +0100

    init cypress comp

---
+++ cypress/support/component.tsx
@@ -0,0 +1,55 @@
+/* eslint-disable @typescript-eslint/no-namespace */
+// ***********************************************************
+// This example support/component.ts is processed and
+// loaded automatically before your test files.
+//
+// This is a great place to put global configuration and
+// behavior that modifies Cypress.
+//
+// You can change the location of this file or turn off
+// automatically serving support files with the
+// 'supportFile' configuration option.
+//
+// You can read more here:
+// https://on.cypress.io/configuration
+// ***********************************************************
+
+import React from "react";
+
+import { mount, MountOptions } from "cypress/react18";
+import { MemoryRouter, MemoryRouterProps } from "react-router-dom";
+import { Provider } from "react-redux";
+// import { store } from "src/store/store";
+
+import "@cypress/code-coverage/support";
+import "./commands";
+import "../../src/index.scss";
+
+declare global {
+  namespace Cypress {
+    interface Chainable {
+      mount: typeof mount;
+    }
+  }
+}
+
+type CustomMountOptions = MountOptions & {
+  routerProps: MemoryRouterProps;
+  //reduxStore: typeof store;
+};
+
+Cypress.Commands.add("mount", (component, options = {}) => {
+  const {
+    routerProps = { initialEntries: ["/"] },
+    //reduxStore = store,
+    ...mountOptions
+  } = options as CustomMountOptions;
+
+  const wrapped = (
+    <Provider store={undefined}>
+      <MemoryRouter {...routerProps}>{component}</MemoryRouter>
+    </Provider>
+  );
+
+  return mount(wrapped, mountOptions);
+});
