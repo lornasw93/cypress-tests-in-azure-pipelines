216d1be91325d44dfb03c8aa149392c43c8bea44
Lorna Watson <lorna@avenue3.com>
2023-07-11 20:48:10 +0100

    pu

--- cypress/e2e/todo.cy.ts
+++ cypress/e2e/todo.cy.ts
@@ -6,7 +6,7 @@ describe('example to-do app', () => {
   })
 
   it('displays two todo items by default', () => {
-    cy.get('.todo-list li').should('have.length', 1);
+    cy.get('.todo-list li').should('have.length', 2);
 
     cy.get('.todo-list li').first().should('have.text', 'Pay electric bill');
     cy.get('.todo-list li').last().should('have.text', 'Walk the dog');
